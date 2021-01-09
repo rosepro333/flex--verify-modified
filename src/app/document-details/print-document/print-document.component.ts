1d875c65d14b8ed035776bcf31780adce8f6bd8e
Md Shamim <mshamimansari111@gmail.com>
2021-01-09 14:34:19 +0530

    working in print document

--- src/app/document-details/print-document/print-document.component.ts
+++ src/app/document-details/print-document/print-document.component.ts
@@ -9,72 +9,76 @@ import html2canvas from 'html2canvas';
   styleUrls: ['./print-document.component.scss']
 })
 export class PrintDocumentComponent implements OnInit {
-//   printData : any = {
-//     Building_No: "42",
-// Document_ID: "5ff2b30114441f4fc06a55c7",
-// ID_Card_Back: "/id_back__1609741156411.jpg",
-// ID_Card_Front: "/id_front_doc82YFgqomRZ_1609741149213.jpg",
-// ID_Type: "Nationality Identify Card",
-// LiveCheck_Photos: ["/livecheck_left_1609741165620.jpg", "/livecheck_up_1609741165628.jpg", "/livecheck_left_1609741165633.jpg", "/livecheck_up_1609741165636.jpg", "/livecheck_center_1609741165638.jpg"],
-// Mode: "Sandbox",
-// Scan_ID: "-zRBlNRDc3",
-// Scaned_Date: "1609741151414",
-// Selfie_Photo: "/livecheck_center_1609741165638.jpg",
-// Tenant_ID: "5fec7c2014441f4fc06a559e",
-// addressStatus: "clear",
-// city: "Bangalore",
-// createdAt: "2021-01-04T06:19:11.415Z",
-// deviceSignature: {browser: "Chrome", browser_version: "87.0.4280.88", deviceType: "desktop", os: "Windows", os_version: "windows-10"},
-// dob: "2021-01-27T18:30:00.000Z",
-// firstName: "Vishal",
-// idCardNo: "123",
-// idCardStatus: "clear",
-// idExpiryDate: "2021-01-27T18:30:00.000Z",
-// ipAddress: "::ffff:127.0.0.1",
-// isActive: true,
-// isDeleted: false,
-// lastName: "Gupta",
-// liveCheckingStatus: "Ok",
-// postalCode: "560068",
-// scanResultComment: "clear",
-// scanResultStatus: "Verified",
-// selfiePhotoMatchStatus: "87.28",
-// streetName: "Btm",
-// updatedAt: "2021-01-04T11:24:25.801Z",
-// updatedDate: null,
-// _id: "5ff2b35f14441f4fc06a55ca",
-// };
-  printData: any = [];
+  printData : any = {
+    Building_No: "42",
+Document_ID: "5ff2b30114441f4fc06a55c7",
+ID_Card_Back: "/id_back__1609741156411.jpg",
+ID_Card_Front: "/id_front_doc82YFgqomRZ_1609741149213.jpg",
+ID_Type: "Nationality Identify Card",
+LiveCheck_Photos: ["/livecheck_left_1609741165620.jpg", "/livecheck_up_1609741165628.jpg", "/livecheck_left_1609741165633.jpg", "/livecheck_up_1609741165636.jpg", "/livecheck_center_1609741165638.jpg"],
+Mode: "Sandbox",
+Scan_ID: "-zRBlNRDc3",
+Scaned_Date: "1609741151414",
+Selfie_Photo: "/livecheck_center_1609741165638.jpg",
+Tenant_ID: "5fec7c2014441f4fc06a559e",
+addressStatus: "clear",
+city: "Bangalore",
+createdAt: "2021-01-04T06:19:11.415Z",
+deviceSignature: {browser: "Chrome", browser_version: "87.0.4280.88", deviceType: "desktop", os: "Windows", os_version: "windows-10"},
+dob: "2021-01-27T18:30:00.000Z",
+firstName: "Vishal",
+idCardNo: "123",
+idCardStatus: "clear",
+idExpiryDate: "2021-01-27T18:30:00.000Z",
+ipAddress: "::ffff:127.0.0.1",
+isActive: true,
+isDeleted: false,
+lastName: "Gupta",
+liveCheckingStatus: "Ok",
+postalCode: "560068",
+scanResultComment: "clear",
+scanResultStatus: "Verified",
+selfiePhotoMatchStatus: "87.28",
+streetName: "Btm",
+updatedAt: "2021-01-04T11:24:25.801Z",
+updatedDate: null,
+_id: "5ff2b35f14441f4fc06a55ca",
+};
+  // printData: any = [];
   constructor(private report: ReportService) {
 
   }
 
   ngOnInit(): void {
     this.report.printData.subscribe((res)=>{
-      this.printData = res;
+      // this.printData = res;
       console.log(res);
     })
   }
-  print = () =>{
+  print = async() =>{
     // var data = document.getElementById('main');
     var data = document.body;
-    html2canvas(data).then(canvas => {
+
+    await html2canvas(data).then((canvas) => {
+      console.log(canvas);
       // Few necessary setting options
       var imgWidth = 208;
       var pageHeight = 295;
       var imgHeight = canvas.height * imgWidth / canvas.width;
       var heightLeft = imgHeight;
 
-      const contentDataURL = canvas.toDataURL('image/png')
+      const contentDataURL =  canvas.toDataURL('image/jpg')
+      console.log(contentDataURL);
       let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
       var position = 0;
+      var image = new Image();
+      image.src = '';
       // const data = 'data:image/jpg;base64,' + btoa(`https://firebasestorage.googleapis.com/v0/b/flexverify.appspot.com/o/id_front_doc82YFgqomRZ_1609741149213.jpg?alt=media`)
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.addImage(contentDataURL, 'JPG', 0, position, imgWidth, imgHeight);
+      console.log(pdf)
       pdf.save('MYPdf.pdf'); // Generated PDF
     });
     // window.print();
   }
-  //  Encodeuint8arr(){
-  //   return new TextEncoder().encode(`https://firebasestorage.googleapis.com/v0/b/flexverify.appspot.com/o/id_front_doc82YFgqomRZ_1609741149213.jpg?alt=media`);
-  // }
+
 }
